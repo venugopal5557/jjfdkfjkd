b9062fb6397afe4a6c6facd6e37bcb64e656cbd2
harish07 <harish07@qedge.com>
2016-08-29 12:40:36 +0530

    hi

---
+++ src/com/stock/tests/Basicscript.java
@@ -0,0 +1,17 @@
+package com.stock.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Basicscript {
+
+	public static void main(String[] args)
+	{
+	WebDriver d=new FirefoxDriver():
+		d.manage().window().maximize();
+	d.get("http://webapp.qedgetech.com");
+		
+
+	}
+
+}
